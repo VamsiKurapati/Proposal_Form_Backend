be5cda45f074cd9e8fbcee3309b69db9f17be393
Vamsi-DraconX <vamsi@draconx.com>
2025-07-25 20:10:53 +0530

    rdcvc

--- routes/Profile.js
+++ routes/Profile.js
@@ -1,17 +1,17 @@
 //Profile.js
 const express = require('express');
 const router = express.Router();
-const verifyUser = require('../utils/verifyUser.js');
+const { verifyUser } = require('../utils/verifyUser');
 
 const { getProfile, updateCompanyProfile, addEmployee, addCaseStudy, addLicenseAndCertification, uploadLogo, getProfileImage, getCaseStudy, addDocument, getDocument } = require('../controllers/profileController.js');
 
-router.get('/getProfile', verifyUser, getProfile);
-router.put('/updateCompanyProfile', verifyUser, updateCompanyProfile);
-router.post('/addEmployee', verifyUser, addEmployee);
-router.post('/addCaseStudy', verifyUser, addCaseStudy);
-router.post('/addLicenseAndCertification', verifyUser, addLicenseAndCertification);
-router.post('/uploadLogo', verifyUser, uploadLogo);
-router.post('/addDocument', verifyUser, addDocument);
+router.get('/getProfile', verifyUser(["company", "editor", "viewer"]), getProfile);
+router.put('/updateCompanyProfile', verifyUser(["company"]), updateCompanyProfile);
+router.post('/addEmployee', verifyUser(["company"]), addEmployee);
+router.post('/addCaseStudy', verifyUser(["company"]), addCaseStudy);
+router.post('/addLicenseAndCertification', verifyUser(["company"]), addLicenseAndCertification);
+router.post('/uploadLogo', verifyUser(["company", "editor", "viewer"]), uploadLogo);
+router.post('/addDocument', verifyUser(["company"]), addDocument);
 router.get('/getProfileImage/file/:id', getProfileImage);
 router.get('/getCaseStudy/:id', getCaseStudy);
 router.get('/getDocument/:id', getDocument);
--- routes/rfpDiscoveryMLModel.js
+++ routes/rfpDiscoveryMLModel.js
@@ -1,20 +1,20 @@
 //rfpDiscoveryMLModel.js
 const express = require('express');
 const router = express.Router();
-const verifyUser = require('../utils/verifyUser');
+const { verifyUser } = require('../utils/verifyUser');
 
 const { getUsersData, matchedRFPData, getAllRFP, save, unsave, getUserandRFPData, generatedProposal, getSavedAndDraftRFPs, saveDraftRFP } = require('../controllers/rfpDiscoveryMLModelController');
 
 router.get('/getUsersData', getUsersData);
 router.post('/matchedRFPdata', matchedRFPData);
-router.get('/getAllRFP', verifyUser, getAllRFP);
-router.post('/saveRFP', verifyUser, save);
-router.post('/unsaveRFP', verifyUser, unsave);
+router.get('/getAllRFP', verifyUser(["company", "editor", "viewer"]), getAllRFP);
+router.post('/saveRFP', verifyUser(["company", "editor", "viewer"]), save);
+router.post('/unsaveRFP', verifyUser(["company", "editor", "viewer"]), unsave);
 
 router.get('/getUserandRFPData', getUserandRFPData);
 router.post('/generatedProposal', generatedProposal);
 
-router.get('/getSavedAndDraftRFPs', verifyUser, getSavedAndDraftRFPs);
-router.post('/saveDraftRFP', verifyUser, saveDraftRFP);
+router.get('/getSavedAndDraftRFPs', verifyUser(["company", "editor", "viewer"]), getSavedAndDraftRFPs);
+router.post('/saveDraftRFP', verifyUser(["company", "editor", "viewer"]), saveDraftRFP);
 
 module.exports = router;
