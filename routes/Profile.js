60beb0f47b83aa8d999b3b36d943bd4eeef34a8d
Vamsi-DraconX <vamsi@draconx.com>
2025-08-02 20:40:09 +0530

    changes

--- routes/Profile.js
+++ routes/Profile.js
@@ -4,11 +4,12 @@ const router = express.Router();
 
 const verifyUser = require('../utils/verifyUser');
 
-const { getProfile, getEmployeeProfile, updateCompanyProfile, updateEmployeeProfile, addEmployee, addCaseStudy, addLicenseAndCertification, uploadLogo, getProfileImage, getCaseStudy, addDocument, getDocument } = require('../controllers/profileController.js');
+const { getProfile, getEmployeeProfile, updateCompanyProfile, updateEmployeeProfile, addEmployee, addCaseStudy, addLicenseAndCertification, uploadLogo, getProfileImage, getCaseStudy, addDocument, getDocument, getProposals } = require('../controllers/profileController.js');
 
 router.get('/getProfile', verifyUser(["company", "employee"]), getProfile);
 router.get('/getEmployeeProfile', verifyUser(["employee"]), getEmployeeProfile);
 router.put('/updateCompanyProfile', verifyUser(["company"]), updateCompanyProfile);
+router.get('/getProposals', verifyUser(["company", "employee"]), getProposals);
 router.put('/updateEmployeeProfile', verifyUser(["employee"]), updateEmployeeProfile);
 router.post('/addEmployee', verifyUser(["company"]), addEmployee);
 router.post('/addCaseStudy', verifyUser(["company"]), addCaseStudy);
