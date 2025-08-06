674696f2728378a2230e4e1500d3c3c271020ed6
Vamsi-DraconX <vamsi@draconx.com>
2025-08-06 17:42:09 +0530

    change

--- controllers/proposalController.js
+++ controllers/proposalController.js
@@ -386,7 +386,7 @@ exports.advancedComplianceCheck = async (req, res) => {
     const proposal_in_array = [initialProposal_1];
     console.log("Proposal in array: ", proposal_in_array);
 
-    const resProposal = await axios.post('http://56.228.64.88:5000/advanced-compliance', initialProposal_1);
+    const resProposal = await axios.post('http://56.228.64.88:5000/advance-compliance', initialProposal_1);
 
     console.log("Response: ", resProposal);
 
