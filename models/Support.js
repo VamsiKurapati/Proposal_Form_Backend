cbb19998f97103c44d93ceb75f836548a7c10fc2
Vamsi-DraconX <vamsi@draconx.com>
2025-08-09 22:36:13 +0530

    changes

--- models/Support.js
+++ models/Support.js
@@ -5,22 +5,22 @@ const supportSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   subject: { type: String, required: true },
   desc: { type: String, required: true },
-  status: { 
-    type: String, 
-    required: true, 
-    enum: ['Open', 'In progress', 'Resolved', 'Closed'],
+  status: {
+    type: String,
+    required: true,
+    enum: ['Open', 'In progress', 'Completed'],
     default: 'Open'
   },
-  
-  priority: { 
-    type: String, 
-    required: true, 
+
+  priority: {
+    type: String,
+    required: true,
     enum: ['Low', 'Medium', 'High'],
     default: 'Low'
   },
-  type: { 
-    type: String, 
-    required: true, 
+  type: {
+    type: String,
+    required: true,
     enum: ['Billing & Payments', 'Proposal issues', 'Account & Access', 'Technical Errors', 'Feature Requests', 'Others'],
     default: 'Others'
   },
