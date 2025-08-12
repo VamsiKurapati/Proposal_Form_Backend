fc3201789346dd32b01f904f0b3a8bf3564b05fb
Vamsi-DraconX <vamsi@draconx.com>
2025-08-12 19:01:05 +0530

    error fix

--- routes/SuperAdmin.js
+++ routes/SuperAdmin.js
@@ -6,32 +6,32 @@ const { getNotificationData, getSupportStatsAndData, updateSupportTicket, create
 
 // router.get('/user_management/getstats', getStats);
 // router.get('/user_management/getcomanyData', getCompanyData);
-router.get('/getCompanyStatsAndData', verifyUser["SuperAdmin"], getCompanyStatsAndData);
-router.put('/updateCompanyStatus/:id', verifyUser["SuperAdmin"], updateCompanyStatus);
+router.get('/getCompanyStatsAndData', verifyUser(["SuperAdmin"]), getCompanyStatsAndData);
+router.put('/updateCompanyStatus/:id', verifyUser(["SuperAdmin"]), updateCompanyStatus);
 
 
-router.get('/getnotificationsData', verifyUser["SuperAdmin"], getNotificationData);
-// router.post('/createNotification', verifyUser["SuperAdmin"], createNotification);
+router.get('/getnotificationsData', verifyUser(["SuperAdmin"]), getNotificationData);
+// router.post('/createNotification', verifyUser(["SuperAdmin"]), createNotification);
 
 
-router.get('/getsupportStatsAndData', verifyUser["SuperAdmin"], getSupportStatsAndData);
-router.put('/updateSupportTicket/:id', verifyUser["SuperAdmin"], updateSupportTicket);
+router.get('/getsupportStatsAndData', verifyUser(["SuperAdmin"]), getSupportStatsAndData);
+router.put('/updateSupportTicket/:id', verifyUser(["SuperAdmin"]), updateSupportTicket);
 
 
 
-router.post('/createSubscriptionPlan', verifyUser["SuperAdmin"], createSubscriptionPlan);
-router.get('/getSubscriptionPlans', verifyUser["SuperAdmin"], getSubscriptionPlans);
-router.put('/updateSubscriptionPlan/:id', verifyUser["SuperAdmin"], updateSubscriptionPlan);
+router.post('/createSubscriptionPlan', verifyUser(["SuperAdmin"]), createSubscriptionPlan);
+router.get('/getSubscriptionPlans', verifyUser(["SuperAdmin"]), getSubscriptionPlans);
+router.put('/updateSubscriptionPlan/:id', verifyUser(["SuperAdmin"]), updateSubscriptionPlan);
 
 
 //payment
-router.get('/getPaymentStatsandData', verifyUser["SuperAdmin"], getPaymentsSummaryAndData);
-router.put('/updatePaymentStatus/:id', verifyUser["SuperAdmin"], updatePaymentStatus);
+router.get('/getPaymentStatsandData', verifyUser(["SuperAdmin"]), getPaymentsSummaryAndData);
+router.put('/updatePaymentStatus/:id', verifyUser(["SuperAdmin"]), updatePaymentStatus);
 // router.get('/payment/getPaymentSummary', getPaymentSummary);
 
 
 //subscription
-router.get('/getSubscriptionData', verifyUser["SuperAdmin"], getSubscriptionData);
+router.get('/getSubscriptionData', verifyUser(["SuperAdmin"]), getSubscriptionData);
 
 
 module.exports = router;
