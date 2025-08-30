b87b306cfbd4ed5fdf413711b6b11a4078cca6b8
Vamsi-DraconX <vamsi@draconx.com>
2025-08-31 00:33:08 +0530

    change

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const dashboardRoute = require('./routes/Dashboard.js');
 const superAdminRoute = require('./routes/SuperAdmin.js');
 const supportRoute = require('./routes/SupportTicket.js');
 const imageRoute = require('./routes/Image.js');
-const stripeRoute = require('./routes/Stripe.js');
+// const stripeRoute = require('./routes/Stripe.js');
 const SubscriptionPlan = require('./models/SubscriptionPlan.js');
 const Subscription = require('./models/Subscription.js');
 const nodemailer = require('nodemailer');
@@ -125,7 +125,7 @@ app.use('/api/support', supportRoute);
 
 app.use('/api/image', imageRoute);
 
-app.use('/api/stripe', stripeRoute);
+// app.use('/api/stripe', stripeRoute);
 
 
 
