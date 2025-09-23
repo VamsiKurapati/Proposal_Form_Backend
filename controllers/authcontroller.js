b76a26f169d06c491c5d6644f981164b77251773
Vamsi-DraconX <vamsi@draconx.com>
2025-09-24 01:32:01 +0530

    resetPassword mail

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -294,6 +294,18 @@ exports.resetPassword = async (req, res) => {
 
     await OTP.deleteOne({ email, otp });
 
+    const subject = "Password Reset Successfully";
+    const body = `
+      Hi ${user.fullName}, <br /><br />
+      Your password has been reset successfully. <br /><br />
+      <strong>Your Login Details:</strong><br />
+      &nbsp;&nbsp;&nbsp;&nbsp;Email: ${user.email}<br />
+      &nbsp;&nbsp;&nbsp;&nbsp;Password: ${newPassword}<br /><br />
+      Best regards,<br />
+      The RFP & Grants Team
+    `;
+    await sendEmail(user.email, subject, body);
+
     res.status(200).json({ message: "Password reset successfully" });
   } catch (err) {
     res.status(500).json({ message: err.message || "Server error" });
