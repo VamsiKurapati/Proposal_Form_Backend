16fbb8fe0d5465fbaaac3a8a7b8de346e605e315
Vamsi-DraconX <vamsi@draconx.com>
2025-10-09 14:28:49 +0530

    Error fix

--- models/OTP.js
+++ models/OTP.js
@@ -4,7 +4,7 @@ const otpSchema = new mongoose.Schema({
     email: { type: String, required: true },
     otp: { type: String, required: true },
     createdAt: { type: Date, default: new Date() },
-    expiresAt: { type: Date, default: () => new Date(Date.now() + 10 * 60 * 1000), index: { expires: 0 } // deletes document once expiresAt is reached}
+    expiresAt: { type: Date, default: () => new Date(Date.now() + 10 * 60 * 1000), index: { expires: 0 } } // deletes document once expiresAt is reached
 }, { timestamps: true });
 
 module.exports = mongoose.model('OTP', otpSchema);
